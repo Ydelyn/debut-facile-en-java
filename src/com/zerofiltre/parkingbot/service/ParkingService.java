c662547d094d1a5a6abf530bb77e407cce62ba90
Lorris <colini.lorris@orange.fr>
2024-08-13 12:53:27 +0200

    p1c8 OK

--- src/com/zerofiltre/parkingbot/service/ParkingService.java
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -2,6 +2,8 @@ package com.zerofiltre.parkingbot.service;
 
 import com.zerofiltre.parkingbot.model.Ticket;
 import com.zerofiltre.parkingbot.model.Vehicle;
+import com.zerofiltre.parkingbot.model.VehicleModelEnum;
+
 import java.util.Date;
 
 public class ParkingService {
@@ -14,4 +16,43 @@ public class ParkingService {
     return ticket;
   }
 
+  public Ticket processExitingVehicle(Ticket ticket) {
+    long now = new Date().getTime();
+    now += 60 * 60 * 1000;
+
+    Date exitTime = new Date(now);
+    ticket.setExitTime(exitTime);
+
+    Vehicle vehicle = ticket.getVehicle();
+    VehicleModelEnum category = vehicle.getCategory();
+
+    double pricePerMinCitadine = 0.08;
+    double pricePerMinDeuxRoues = 0.02;
+    double defaultPricePerMin = 0.05;
+
+    long durationInMinutes = (ticket.getExitTime().getTime() - ticket.getEnteringTime().getTime()) / (1000 * 60);
+    double finalPrice = 0;
+
+//    if ("CITADINE".equals(category)) {
+//      finalPrice = durationInMinutes * pricePerMinCitadine;
+//    } else if ("2 ROUES".equals(category)) {
+//      finalPrice = durationInMinutes * pricePerMinDeuxRoues;
+//    } else {
+//      finalPrice = durationInMinutes * defaultPricePerMin;
+//    }
+
+    switch (category) {
+      case CITADINE:
+        finalPrice = durationInMinutes * pricePerMinCitadine;
+        break;
+      case DEUXROUES:
+        finalPrice = durationInMinutes * pricePerMinDeuxRoues;
+        break;
+      default:
+        finalPrice = durationInMinutes * defaultPricePerMin;
+    }
+
+    ticket.setAmount(finalPrice);
+    return ticket;
+  }
 }
