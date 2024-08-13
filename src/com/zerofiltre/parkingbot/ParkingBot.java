1041b1c24a76c36cb9c6de91812dd74f5a6d1d34
Lorris <colini.lorris@orange.fr>
2024-08-13 11:08:35 +0200

    p1c6 OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,5 +1,9 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.model.Ticket;
+import com.zerofiltre.parkingbot.model.Vehicle;
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
 
@@ -9,8 +13,17 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
+    processVehicles();
   }
 
+  private static void processVehicles() {
+    Vehicle vehicle = new Vehicle();
+    vehicle.setRegistrationNumber("LS-324-PM");
+    vehicle.setCategory("CITADINE");
+    ParkingService parkingService = new ParkingService();
+    Ticket ticket = parkingService.processIncomingVehicle(vehicle);
+    System.out.println(ticket);
+  }
 
 
 }
