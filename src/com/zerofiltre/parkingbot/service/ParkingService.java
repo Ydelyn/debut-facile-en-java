89069c6404bd20e698f4eab731cb86cd5422734f
Lorris <colini.lorris@orange.fr>
2024-08-13 19:47:03 +0200

    p1c11 OK

--- src/com/zerofiltre/parkingbot/service/ParkingService.java
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -1,7 +1,11 @@
 package com.zerofiltre.parkingbot.service;
 
+import com.zerofiltre.parkingbot.model.Parking;
+import com.zerofiltre.parkingbot.model.ParkingTypeEnum;
 import com.zerofiltre.parkingbot.model.Ticket;
 import com.zerofiltre.parkingbot.model.Vehicle;
+
+import java.security.SecureRandom;
 import java.util.Date;
 
 public class ParkingService {
@@ -44,4 +48,39 @@ public class ParkingService {
 
   }
 
+  public Parking initParking() {
+    // Initialisation of the Building Parking
+    Parking building = new Parking();
+    building.setType(ParkingTypeEnum.BUILDING);
+    building.setNumber(1);
+
+    // Initialisation of Floors type Parking
+    for (int i = 1; i <= 50; i++) {
+      Parking floor = new Parking();
+      floor.setType(ParkingTypeEnum.FLOOR);
+      floor.setNumber(i);
+      building.getSubParking().add(floor);
+
+      // Initialisation of Halls type Parking
+      for (int j = 1; j <= 30; j++) {
+        Parking hall = new Parking();
+        hall.setType(ParkingTypeEnum.HALL);
+        hall.setNumber(j);
+        floor.getSubParking().add(hall);
+
+        // Initialisation of Spots type Parking
+        for (int k = 1; k <= 20; k++) {
+          Parking spot = new Parking();
+          spot.setType(ParkingTypeEnum.SPOT);
+          spot.setNumber(k);
+          Vehicle vehicle = new Vehicle();
+          vehicle.setRegistrationNumber(String.valueOf(new SecureRandom().nextInt(10000)));
+          vehicle.setParkingSpotNumber("1 - " + i + " - " + j + " - " + k );
+          spot.setVehicle(vehicle);
+          hall.getSubParking().add(spot);
+        }
+      }
+    }
+    return building;
+  }
 }
