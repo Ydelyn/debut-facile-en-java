1ba6e2308be0396e46c4443304651f422ae07ee2
Lorris <colini.lorris@orange.fr>
2024-08-14 16:08:57 +0200

    p2c3 OK

--- src/main/java/com/zerofiltre/parkingbot/service/ParkingService.java
+++ src/main/java/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -2,7 +2,12 @@ package com.zerofiltre.parkingbot.service;
 
 import com.zerofiltre.parkingbot.model.Ticket;
 import com.zerofiltre.parkingbot.model.Vehicle;
+import com.zerofiltre.parkingbot.model.VehiculeCategoryEnum;
+
 import java.util.Date;
+
+import static com.zerofiltre.parkingbot.model.VehiculeCategoryEnum.BICYCLE;
+import static com.zerofiltre.parkingbot.model.VehiculeCategoryEnum.CITADINE;
 
 public class ParkingService {
 
@@ -12,4 +17,29 @@ public class ParkingService {
     ticket.setEnteringTime(new Date());
     return ticket;
   }
+
+    public Ticket processExitingVehicle(Ticket ticket) {
+      ticket.setExitTime(new Date());
+
+      Vehicle vehicle = ticket.getVehicle();
+      VehiculeCategoryEnum category = vehicle.getCategory();
+
+      double pricePerMinForCitadine = 0.08;
+      double pricePerMinFor2Roues = 0.02;
+      double defaultPricePerMinFor = 0.05;
+      long durationInMinutes = (ticket.getExitTime().getTime() - ticket.getEnteringTime().getTime()) / (60 * 1000);
+
+      double finalPrice;
+
+      if(CITADINE.equals(category)) {
+        finalPrice = durationInMinutes * pricePerMinForCitadine;
+      } else if (BICYCLE.equals(category)) {
+        finalPrice = durationInMinutes * pricePerMinFor2Roues;
+      } else {
+        finalPrice = durationInMinutes * defaultPricePerMinFor;
+      }
+      ticket.setAmount(finalPrice);
+
+      return ticket;
+    }
 }
--- src/test/java/com/zerofiltre/parkingbot/service/ParkingServiceTest.java
+++ src/test/java/com/zerofiltre/parkingbot/service/ParkingServiceTest.java
@@ -2,25 +2,69 @@ package com.zerofiltre.parkingbot.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.zerofiltre.parkingbot.model.Bicycle;
+import com.zerofiltre.parkingbot.model.Car;
 import com.zerofiltre.parkingbot.model.Ticket;
 import com.zerofiltre.parkingbot.model.Vehicle;
 import java.util.Date;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParkingServiceTest {
 
   public static final String REGISTRATION_NUMBER = "AZ-458-56";
+  public static final String CAR_REGISTRATION_NUMBER = "AA";
+  private static final String BICYCLE_REGISTRATION_NUMBER = "GTFSHSJ";
 
   ParkingService parkingService = new ParkingService();
+  private Vehicle vehicle;
+  private Ticket ticket;
+  private Date now;
+  private Date enteringTime;
 
+
+  @BeforeAll
+  void init() {
+    //given : Soit un véhicule a l'entrée du parking avec une plaque d'immatriculation X
+
+    vehicle = new Vehicle();
+    vehicle.setRegistrationNumber(REGISTRATION_NUMBER);
+
+    ticket = new Ticket();
+    ticket.setVehicle(vehicle);
+  }
+
+  @BeforeEach
+  void reInit() {
+    now = new Date();
+    long nowMinus1Hour = now.getTime() - 60 * 60 * 1000;
+    enteringTime = new Date(nowMinus1Hour);
+    ticket.setEnteringTime(enteringTime);
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("Je m'affiche à chaque fin de test");
+  }
+
+  @AfterAll
+  void afterAll() {
+    System.out.println("Je m'affiche à la fin de tous les tests");
+  }
+
+  @Timeout(2)
+  @Test
+  void failMoreThan2Seconds() {
+    try {
+      Thread.sleep(3 * 1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
   @Test
   void givenAVehicle_processIncomingVehicle_generatesTicketWithRightTime() {
-    //given : Soit un véhicule a l'entrée du parking avec une plaque d'immatriculation X
-    Vehicle vehicle = new Vehicle();
-    vehicle.setRegistrationNumber(REGISTRATION_NUMBER);
-    Date now = new Date();
-
     //when : Enregistrer le véhicule
     Ticket ticket = parkingService.processIncomingVehicle(vehicle);
 
@@ -41,5 +85,46 @@ public class ParkingServiceTest {
     assertThat(date).isBeforeOrEqualTo(nowPlus30Seconds);
   }
 
+  @Test
+  void givenARegisteredVehicle_processExitingVehicle_generatesTicketWithHourAndPrice() {
+    // when
+    Ticket exitTicket = parkingService.processExitingVehicle(ticket);
+
+    //then
+    assertThat(exitTicket).isNotNull();
+    Date exitTime = exitTicket.getExitTime();
+    assertThat(exitTime).isNotNull();
+    assertThat(exitTime).isAfter(ticket.getEnteringTime());
+    assertThat(exitTicket.getAmount()).isNotEqualTo(0);
+  }
+
+  @Test
+  void givenARegisteredVehicle_processExitingVehicle_generatesTheRightPrice() {
+    Vehicle car = new Car();
+    car.setRegistrationNumber(CAR_REGISTRATION_NUMBER);
+    Ticket carTicket = new Ticket();
+    carTicket.setVehicle(car);
+
+    Vehicle bicycle = new Bicycle();
+    bicycle.setRegistrationNumber(BICYCLE_REGISTRATION_NUMBER);
+    Ticket bicycleTicket = new Ticket();
+    bicycleTicket.setVehicle(bicycle);
+
+    carTicket.setEnteringTime(enteringTime);
+    bicycleTicket.setEnteringTime(enteringTime);
+
+    // then
+    Ticket exitVehicleTicket = parkingService.processExitingVehicle(ticket);
+    Ticket exitCarTicket = parkingService.processExitingVehicle(carTicket);
+    Ticket exitBicycleTicket = parkingService.processExitingVehicle(bicycleTicket);
+
+    assertThat(exitVehicleTicket).isNotNull();
+    assertThat(exitCarTicket).isNotNull();
+    assertThat(exitBicycleTicket).isNotNull();
+
+    assertThat(exitVehicleTicket.getAmount()).isEqualTo(3);
+    assertThat(exitCarTicket.getAmount()).isEqualTo(4.8);
+    assertThat(exitBicycleTicket.getAmount()).isEqualTo(1.2);
+  }
 
 }
